75d0854bc021e393f4e1fab5be8569d133965e65
Ilpo Ruotsalainen <lonewolf@iki.fi>
2013-03-28 13:50:51 +0200

    Fix pwm_isr to work when recursed.

--- globals.h
+++ globals.h
@@ -11,20 +11,10 @@
 
 #ifdef __ASSEMBLER__
 
-#define pwm_SREG_save r2
-#define pwm_scratch1  r3
-#define pwm_scratch2  r4
-#define pwm_scratch3  r5
 #define pwm_write_pos r16
 #define pwm_read_pos  r17
 
 #else
-
-/* Registers reserved for PWM interrupt handler */
-register uint8_t pwm_SREG_save asm("r2");
-register uint8_t pwm_scratch1 asm("r3");
-register uint8_t pwm_scratch2 asm("r4");
-register uint8_t pwm_scratch3 asm("r5");
 
 volatile register uint8_t pwm_write_pos asm("r16");
 volatile register uint8_t pwm_read_pos asm("r17");
